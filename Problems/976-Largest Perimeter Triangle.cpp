43bb31523b3a969b324a4e41900a6e872fa22d52
KirkGuo <55413682+KirkGuo@users.noreply.github.com>
2019-09-17 11:10:58 -0400

    Create 976-Largest Perimeter Triangle.cpp

---
+++ Problems/976-Largest Perimeter Triangle.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& A) {
+        sort(A.begin(), A.end());
+        for(int i=A.size()-3; i>=0; i--)
+            if(A[i+2]<A[i+1]+A[i])
+                return A[i] + A[i+1] + A[i+2];
+        return 0;
+    }
+};
